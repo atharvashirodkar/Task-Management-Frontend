23d40c85abf88518914d3aa444d9704407112807
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-03-06 11:32:11 +0530

    updated search component

--- src/component/TaskSearch.jsx
+++ src/component/TaskSearch.jsx
@@ -1,49 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { Box, TextField, InputAdornment, IconButton, Paper, Typography } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-
+import React, { useState } from "react";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const TaskSearch = ({ onSearch }) => {
   const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    const debounceTimer = setTimeout(() => onSearch(query), 500);
-    return () => clearTimeout(debounceTimer);
-  }, [query, onSearch]);
+  const handleSearch = () => {
+    onSearch(query); // Call the onSearch prop with the search query
+  };
 
   return (
-    <Box>
-        <TextField
-          fullWidth
-          placeholder="Type to search..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconButton>
-                  <SearchIcon sx={{ color: 'black' }} />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-          sx={{
-            backgroundColor: 'rgba(255, 209, 209, 0.1)',
-            borderRadius: 10,
-            border: 2,
-            color: 'black',
-            '& .MuiOutlinedInput-notchedOutline': {
-              borderColor: 'rgba(255, 255, 255, 0.3)',
-            },
-            '&:hover .MuiOutlinedInput-notchedOutline': {
-              borderColor: 'rgba(0, 98, 255, 0.6)',
-            },
-            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-              borderColor: 'white',
-            },
-          }}
-        />
-    </Box>
+    <TextField
+      fullWidth
+      variant="outlined"
+      placeholder="Search tasks..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      onKeyPress={(e) => {
+        if (e.key === "Enter") {
+          handleSearch();
+        }
+      }}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton onClick={handleSearch}>
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
   );
 };
 
