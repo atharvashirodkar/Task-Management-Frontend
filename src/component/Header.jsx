dc266f2f8d64a599637cd4a96b5bee31c5ed7805
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-03-06 11:15:37 +0530

    Added register, login & logout logic

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,13 +1,55 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('user');
+    // console.log(token);
+    setIsAuthenticated(!!token);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setIsAuthenticated(false);
+    navigate("/login");
+  };
+
   return (
     <Box sx={{ flexGrow: 1, marginBottom: 2 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontSize: { xs: "1rem", sm: "1.25rem", md: "1.5rem" } }}>
+          <Typography
+            variant="h6"
+            component={Link}
+            to="/"
+            sx={{
+              flexGrow: 1,
+              fontSize: { xs: "1rem", sm: "1.25rem", md: "1.5rem" },
+              textDecoration: "none",
+              color: "inherit",
+            }}
+          >
             Task Management
           </Typography>
+
+          {isAuthenticated ? (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/register">
+                Register
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
